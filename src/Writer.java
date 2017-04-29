a771aea04e13e005dfee983359f3328fe09e1d1b
cwgroppe <groppec1@webmail.nku.edu>
2017-04-29 10:15:26 -0400

    Code takes about two minutes to run, also butts.

--- src/Writer.java
+++ src/Writer.java
@@ -1,5 +1,45 @@
 /**
  * Created by Corey on 4/23/17.
  */
-public class Writer {
+public class Writer extends Thread implements Runnable{
+
+    @Override
+    public synchronized void run(){
+        writer();
+    }
+
+    public void writer(){
+        do{
+            Driver.wmut.acquireUninterruptibly();//get mutex for wcount
+            Driver.wcount++;
+            System.out.println("Writer want data.");
+            Driver.wmut.release();
+
+            if(Driver.wcount > 1 || Driver.rcount > 0){
+                Driver.want.acquireUninterruptibly();
+            }
+
+            System.out.println("Writer done did the write.");
+            //preform write here
+            Driver.wmut.acquireUninterruptibly();
+            System.out.println("Writer done with data.");
+            Driver.wcount--;//writer is done
+
+            if(Driver.wcount > 0){
+                Driver.want.release();
+            }else{
+                Driver.rmut.acquireUninterruptibly();
+                while(Driver.rcount > 0){
+                    Driver.want.release();//let readers in
+                    Driver.rcount--;
+                }//end while
+            }//end if
+
+            Driver.wmut.release();//reader time
+            Driver.rmut.release();//clean up and leave
+
+            Driver.nap();
+        }while(true);//end do
+    }
+
 }
